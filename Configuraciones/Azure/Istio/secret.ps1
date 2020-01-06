5dee668d3295b6a0f74920ee4538b922c157d122
Eugenio Garcia San Martin <egsmartin@gmail.com>
2020-01-06 16:49:41 +0100

    Recursos Istio

---
+++ Configuraciones/Azure/Istio/secret.ps1
@@ -0,0 +1,1 @@
+kubectl create secret docker-registry milocalregistry --docker-server=egsmcontenedor.azurecr.io --docker-username=egsmContenedor --docker-password=LNTuvGUKsCf9w5lMgJwYBjiMWJL/19Ea -n heroes-ns
