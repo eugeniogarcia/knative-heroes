bcacc074b58993a8f2900b7c3c187982365fc33c
Eugenio Garcia San Martin <egsmartin@gmail.com>
2020-03-15 20:47:10 +0100

    Knative

---
+++ Configuraciones/Azure/KNative/secret.ps1
@@ -0,0 +1,1 @@
+kubectl create secret docker-registry milocalregistry --docker-server=pruebacontenedor.azurecr.io --docker-username=pruebacontenedor --docker-password=Pr0bDGtfdIKbWj+pGbGEsFpc8D/3enAH -n default
